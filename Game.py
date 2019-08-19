9e19e8bfa980354b621b4eea2418a6b274bf5cff
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-19 15:18:17 +0300

    add player class

--- Game.py
+++ Game.py
@@ -1,8 +1,20 @@
 import tkinter as tk
 import random
 
-def randomPoz(top):
-    return random.randint(1, top-1)*step
+class Player:
+
+    def __init__(this, color):
+        this.x = this.randomPoz(N_X)
+        this.y = this.randomPoz(N_Y)
+        this.color = color
+
+    def draw(this):
+        canvas.create_oval((this.x, this.y),
+                           (this.x+step, this.y+step),
+                           fill=this.color)
+
+    def randomPoz(this, top):
+        return random.randint(1, top-1)*step
 
 master = tk.Tk()
 step = 60
@@ -10,6 +22,10 @@ N_X = 10
 N_Y = 10
 canvas = tk.Canvas(master, bg="blue", height = step*N_X, width=step*N_Y)
 
-player_pos = (randomPoz(N_X), randomPoz(N_Y))
-exit_pos = (randomPoz(N_X), randomPoz(N_Y))
+player = Player("green")
+player.draw();
+exit_g = Player("yellow")
+exit_g.draw();
 
+canvas.pack()
+master.mainloop()
