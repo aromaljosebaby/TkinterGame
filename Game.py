412f2786df71a99a452e2e46fd920fd47a367c1c
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-22 15:26:02 +0300

    end of project

--- Game.py
+++ Game.py
@@ -2,10 +2,15 @@ import tkinter as tk
 import random
 
 class Player:
+    
+    objects = {(-1,-1)}
 
     def __init__(this, color):
-        this.x = this.randomPoz(N_X)
-        this.y = this.randomPoz(N_Y)
+        this.x, this.y = -1, -1
+        while (this.x, this.y) in Player.objects:
+            this.x = this.randomPoz(N_X)
+            this.y = this.randomPoz(N_Y)
+        Player.objects.add((this.x, this.y))
         this.color = color
         this.draw()
 
@@ -18,15 +23,34 @@ class Player:
         return random.randint(1, top-1)*step
 
     def repaint(this, x, y):
-        canvas.move(this.body, x, y)
+        old_x, old_y = this.x, this.y
+        this.x = (this.x + x)%(step*N_X)
+        this.y = (this.y + y)%(step*N_Y)
+        canvas.move(this.body, (this.x - old_x), (this.y - old_y))
 
 class Exit(Player):
     def __init__(this):
         super().__init__("yellow")
 
 class Enemy(Player):
+    def __init__(this, color = "red"):
+        super().__init__(color)
+
+class EnemyD(Enemy):
+
     def __init__(this):
-        super().__init__("red")
+        super().__init__("pink")
+
+    def randomStep(this):
+        r = random.randint(1, 4)
+        if r == 1:
+            super().repaint(step, 0)
+        elif r == 2:
+            super().repaint(-step, 0)
+        elif r == 3:
+            super().repaint(0,step)
+        else:
+            super().repaint(0,-step)
 
 class Hero(Player):
     def __init__(this):
@@ -37,31 +61,45 @@ class Hero(Player):
 
 def keyPress(event):
     print(event)
-    if event.keycode == 37:     #left
-        player.x -= step
+    keys = {37, 38, 39, 40}
+    if event.keycode in keys:
+        keyListener(event.keycode)
+        enemiesStep()
+        endGame()
+
+def keyListener(key):
+    if key == 37:     #left
         player.repaint(-step, 0)
-    elif event.keycode == 38:   #up
-        player.y -= step
+    elif key == 38:   #up
         player.repaint(0, -step)
-    elif event.keycode == 39:   #right
-        player.x += step
+    elif key == 39:   #right
         player.repaint(step, 0)
-    elif event.keycode == 40:   #down
-        player.y += step
+    elif key == 40:   #down
         player.repaint(0, step)
-    endGame()
+    
+def enemiesStep():
+    for enemy in enemies_d:
+        enemy.randomStep()
+
 
 def endGame():
-    print("player:",player.x, player.y)
-    print("exit_g:",exit_g.x, exit_g.y)
     if player.checkPos(exit_g):
         print("Game over")
         print("You won!!!")
+    enemies = enemies_d + enemies_s
+    for enemy in enemies:
+        if player.checkPos(enemy):
+            print("Game over")
+            print("You lose!!!")
+            break
 
 def addEnemies():
     for i in range(6):
         enemy = Enemy()
         enemies_s.append(enemy)
+    for i in range(3):
+        enemy = EnemyD()
+        enemies_d.append(enemy)
 
 
 master = tk.Tk()
@@ -69,7 +107,11 @@ step = 60
 N_X = 10
 N_Y = 10
 enemies_s = []
+enemies_d = []
 canvas = tk.Canvas(master, bg="blue", height = step*N_X, width=step*N_Y)
+
+player_image = tk.PhotoImage(file="images/fr.gif")
+canvas.create_image((0,0), player_image, anchor="nw")
 
 addEnemies()
 exit_g = Exit()
